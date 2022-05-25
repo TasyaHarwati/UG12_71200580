ec2b7308bbea70d47e98b5cee428c33fb66bf741
TasyaHarwati <79096171+TasyaHarwati@users.noreply.github.com>
2022-05-25 18:56:47 +0700

    Add files via upload

---
+++ UG12-71200580-ProjectKassa/src/main/java/com/ug12/projectkassa/Kasir.java
@@ -0,0 +1,9 @@
+package com.ug12.projectkassa;
+
+import java.lang.reflect.Array;
+
+public class Kasir {
+    private Integer pesanan;
+    private Array kasir;
+    private String kasir;
+}
---
+++ UG12-71200580-ProjectKassa/src/main/java/com/ug12/projectkassa/Kassa.java
@@ -0,0 +1,24 @@
+package com.ug12.projectkassa;
+
+public class Kassa {
+    private long totalPenjualan;
+    private String password;
+    private String username;
+    private String nama;
+
+    public long getTotalPenjualan() {
+        return totalPenjualan;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+}
